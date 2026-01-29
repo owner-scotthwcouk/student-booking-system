62bc2d4981853e3bffe974638107e05acd27f391
Scott <scott@scott-hw.online>
2026-01-29 15:59:48 +0000

    v

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { useAuth } from 'src\contexts\auth.jsx'
+import { AuthProvider } from './contexts/auth.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
