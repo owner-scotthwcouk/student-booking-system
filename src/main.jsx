3af186a5d6b703cdf4e6870b8aff28a9952ed462
Scott <scott@scott-hw.online>
2026-06-08 17:17:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { AuthProvider } from './contexts/auth.jsx';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+// Ensure you only load PayPal here, NOT Stripe
+const paypalOptions = {
+  "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID_SANDBOX, // Use the appropriate Env Var
+  currency: "GBP",
+  intent: "capture",
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PayPalScriptProvider options={paypalOptions}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </PayPalScriptProvider>
   </React.StrictMode>,
-)
+);
