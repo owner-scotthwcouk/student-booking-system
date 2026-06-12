9f5235cef4dbc85befdef9a9db5fec2664913576
Scott <scott@scott-hw.online>
2026-06-12 07:39:14 +0100

    Install Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import './App.css'
-import { inject } from '@vercel/analytics'
-
-inject()
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './contexts/auth.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Analytics />
     </AuthProvider>
   </React.StrictMode>,
 );
