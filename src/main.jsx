303a43092e660d62ef7edd5e9207ecc70e810b74
Scott <scott@scott-hw.online>
2026-07-10 19:10:45 +0100

    Redesigned the shared interface around #8727F5

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './design-system.css';
 import { AuthProvider } from './contexts/auth.jsx';
 import { Analytics } from '@vercel/analytics/react';
 
